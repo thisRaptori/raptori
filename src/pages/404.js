2192f17af2f94dbf70115e2fb77051e89d9cfeb8
Joe McGrath <joe.mcgrath@fanduel.com>
2019-03-10 00:05:10 +0000

    lowercase title

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { Layout, SEO } from 'src/components'
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
+		<h1>Not Found</h1>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</Layout>
 )
