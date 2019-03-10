976ec321441b781d561af88600d7c14d2880a434
Joe McGrath <joe.mcgrath@fanduel.com>
2019-03-10 01:14:57 +0000

    styling and text tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ const IndexPage = ({
 					title="Home"
 					keywords={[`gatsby`, `application`, `react`]}
 				/>
-				<h1>Hey, I'm Joe McGrath!</h1>
+				<h1>Hello!</h1>
 				<p>
-					I'm a senior software engineer with a background in design,
-					currently working at{' '}
+					I'm Joe McGrath, a senior software engineer with a
+					background in design, currently working at{' '}
 					<Link to="https://www.fanduel.com">FanDuel</Link>. I build
 					highly scaleable web apps, contribute to open source, and
 					write fiction in my spare time!
