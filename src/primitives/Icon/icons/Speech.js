49c10712e213884fe08ad9d986f5444fbb92cee9
Joe McGrath <joe.mcgrath@fanduel.com>
2019-03-10 01:37:53 +0000

    make svgs react to interaction

--- src/primitives/Icon/icons/Speech.js
+++ src/primitives/Icon/icons/Speech.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import JumpSvg from '../Jump'
+
 const Speech = props => (
-	<svg {...props} viewBox="0 -1 16 17">
+	<JumpSvg {...props} viewBox="0 -1 16 17">
 		<ellipse cx="8" cy="6.5" rx="8" ry="6.5" />
 		<polygon points="1 15 3 10 5 12" />
-	</svg>
+	</JumpSvg>
 )
 
 export default Speech
