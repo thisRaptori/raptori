956bb9296012dd4264206541cfbbd225cb4f0288
Joe McGrath <joe.mcgrath@fanduel.com>
2019-03-14 19:33:39 +0000

    make sun/moon icons jump

--- src/primitives/Icon/icons/Sun.js
+++ src/primitives/Icon/icons/Sun.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import JumpSvg from '../Jump'
+
 const Sun = props => (
-	<svg {...props} viewBox="-0.5 -0.5 16 16" strokeLinecap="round">
+	<JumpSvg {...props} viewBox="-0.5 -0.5 16 16" strokeLinecap="round">
 		<circle cx="7.5" cy="7.5" r="3.40909091" />
 		<path d="M7.5,0 L7.5,1.36363636" />
 		<path d="M7.5,13.6363636 L7.5,15" />
@@ -11,7 +13,7 @@ const Sun = props => (
 		<path d="M13.6363636,7.5 L15,7.5" />
 		<path d="M2.19545455,12.8045455 L3.16363636,11.8363636" />
 		<path d="M11.8363636,3.16363636 L12.8045455,2.19545455" />
-	</svg>
+	</JumpSvg>
 )
 
 Sun.defaultProps = {
