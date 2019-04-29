bc033e10512df633d7a51b56d0af335bace36051
Joe McGrath <joe.mcgrath@fanduel.com>
2019-04-29 22:29:40 +0100

    move index latest posts section out of footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,37 +7,28 @@ const IndexPage = ({
 		allMarkdownRemark: { edges },
 	},
 }) => (
-	<Layout
-		children={
-			<>
-				<SEO title="Home" />
-				<WaveSection>
-					<h1>Hello!</h1>
-					<p>
-						I'm Joe McGrath, a senior software engineer with a
-						background in design, currently working at{' '}
-						<Link to="https://webflow.com/">Webflow</Link>. I build
-						highly scaleable web apps, contribute to open source,
-						and write fiction in my spare time!
-					</p>
-					<p>
-						<Link to="/resume/" button>
-							View Resume
-						</Link>
-					</p>
-				</WaveSection>
-				<h3>Selected Projects</h3>
-			</>
-		}
-		belowTheWaves={
-			<>
-				<h3>Latest Posts</h3>
-				{edges.map(edge => (
-					<PostLink key={edge.node.id} post={edge.node} inFooter />
-				))}
-			</>
-		}
-	/>
+	<Layout>
+		<SEO title="Home" />
+		<WaveSection>
+			<h1>Hello!</h1>
+			<p>
+				I'm Joe McGrath, a senior software engineer with a background in
+				design, currently working at{' '}
+				<Link to="https://webflow.com/">Webflow</Link>. I build highly
+				scaleable web apps, contribute to open source, and write fiction
+				in my spare time!
+			</p>
+			<p>
+				<Link to="/resume/" button>
+					View Resume
+				</Link>
+			</p>
+		</WaveSection>
+		<h3>Latest Posts</h3>
+		{edges.map(edge => (
+			<PostLink key={edge.node.id} post={edge.node} />
+		))}
+	</Layout>
 )
 
 export default IndexPage
