76d4a3779f265471bd04bdf995bce4bc3cac269d
Joe McGrath <joe.mcgrath@fanduel.com>
2019-05-03 18:49:14 +0100

    make waves more dynamic

--- src/components/Waves/Waves.js
+++ src/components/Waves/Waves.js
@@ -9,9 +9,11 @@ const Waves = ({ invert, offset }) => {
 	return (
 		<Wrapper className="background transition" invert={invert}>
 			<Gradient />
-			<Wave offset={offset} fill={primary} speed={0.8} size={1.1} />
-			<Wave offset={offset} fill={primary} speed={1.2} size={1} />
-			<Wave offset={offset} fill={primary} speed={1.6} size={0.9} />
+			<Wave offset={offset} fill={primary} speed={0.8} size={1.0} />
+			<Wave offset={offset} fill={primary} speed={1.1} size={1.1} />
+			<Wave offset={offset} fill={primary} speed={1.2} size={0.9} />
+			<Wave offset={offset} fill={primary} speed={1.3} size={0.7} />
+			<Wave offset={offset} fill={primary} speed={1.6} size={0.8} />
 		</Wrapper>
 	)
 }
