b025d720af240d1b265e5e82a4c11c59711d8602
Joe McGrath <joe.mcgrath@fanduel.com>
2019-05-05 20:34:13 +0100

    tweak ThemeToggle alignment

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Icon } from 'src/components'
 import { useRaptoriTheme } from 'src/hooks'
+
+const Button = styled.button`
+	svg {
+		margin-bottom: 4px;
+	}
+`
 
 const ThemeToggle = () => {
 	const [isDark, toggleTheme] = useRaptoriTheme()
 
 	return (
-		<button onClick={toggleTheme}>
+		<Button onClick={toggleTheme}>
 			<Icon name={isDark ? 'Sun' : 'Moon'} />
-		</button>
+		</Button>
 	)
 }
 
