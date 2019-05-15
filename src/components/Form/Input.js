3741bf7d6a9fee13f9a3bdf169ae9436a3ec36a1
Joe McGrath <joe@jmcgrath.co.uk>
2019-05-15 22:47:38 +0100

    tweak input styling

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -4,22 +4,23 @@ import styled from 'styled-components'
 const P = styled.p`
 	label {
 		display: flex;
+		flex-direction: column;
 	}
 
 	input {
 		background: transparent;
-		border: 1px solid currentColor;
-		border-radius: 4px;
+		border: none;
+		border-bottom: 1px solid currentColor;
 		color: inherit;
 		flex: 1 1 100%;
-		font-size: inherit;
-        margin: 0 0.5em;
-		padding: 0.2em 0.5em;
-        
-        ::placeholder {
-            color: inherit;
-            opacity: .5;
-        }
+		font-size: 0.8em;
+		margin: 0;
+		padding: 0.5em 0;
+
+		::placeholder {
+			color: inherit;
+			opacity: 0.5;
+		}
 	}
 `
 
