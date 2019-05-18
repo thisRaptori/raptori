edfc5b533fe1afca9a954cc4b08fc7eabfafaeca
Joe McGrath <joe@jmcgrath.co.uk>
2019-05-18 03:54:50 +0100

    handle case where user is holding phone differently

--- src/scripts/tilt.js
+++ src/scripts/tilt.js
@@ -1,7 +1,9 @@
 ;(function() {
 	const gatsbyRoot = document.getElementById('___gatsby')
 	const rotationHandler = event => {
-		const skewValue = -Math.min(Math.max(-30, event.gamma), 30)
+		const skewStrength = Math.min(Math.max(-30, event.gamma), 30)
+		// flip the number when the phone is facing towards the user
+		const skewValue = event.alpha > 90 ? skewStrength : -skewStrength
 		gatsbyRoot.style.setProperty('--skew', `${skewValue}deg`)
 	}
 	window.addEventListener('deviceorientation', rotationHandler, true)
