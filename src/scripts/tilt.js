04d76c65b79a163ff17100ce7bbe9a888ddb79e9
Joe McGrath <joe@raptori.dev>
2019-05-24 04:56:02 +0100

    throttle tilt

--- src/scripts/tilt.js
+++ src/scripts/tilt.js
@@ -1,6 +1,13 @@
 ;(function() {
+	let throttle
 	const gatsbyRoot = document.getElementById('___gatsby')
 	const rotationHandler = event => {
+		if (throttle) {
+			return
+		}
+		throttle = setTimeout(() => {
+			throttle = undefined
+		}, 32)
 		const skewStrength = Math.min(Math.max(-30, event.gamma), 30)
 		// flip the number when the phone is facing towards the user
 		const skewValue = event.beta > 90 ? skewStrength : -skewStrength
