d741914293a6048baab3af349d52979ebdef0b23
Joe McGrath <joe@raptori.dev>
2019-05-25 17:25:18 +0100

    make tilt code clearer

--- src/scripts/tilt.js
+++ src/scripts/tilt.js
@@ -1,27 +1,33 @@
 ;(function() {
-	let waitUntilScrollEnd
 	let throttle
+	let userIsScrolling
 	const gatsbyRoot = document.getElementById('___gatsby')
+
+	const unsetIsScrolling = () => {
+		userIsScrolling = undefined
+	}
+	const unsetThrottle = () => {
+		throttle = undefined
+	}
+
 	const rotationHandler = event => {
-		if (waitUntilScrollEnd || throttle) {
+		if (userIsScrolling || throttle) {
 			return
 		}
-		throttle = setTimeout(() => {
-			throttle = undefined
-		}, 32)
+		throttle = setTimeout(unsetThrottle, 32)
 		const skewStrength = Math.min(Math.max(-30, event.gamma), 30)
 		// flip the number when the phone is facing towards the user
 		const skewValue = event.beta > 90 ? skewStrength : -skewStrength
 		gatsbyRoot.style.setProperty('--skew', `${skewValue}deg`)
 	}
+
 	const scrollHandler = () => {
-		if (waitUntilScrollEnd) {
-			clearTimeout(waitUntilScrollEnd)
+		if (userIsScrolling) {
+			clearTimeout(userIsScrolling)
 		}
-		waitUntilScrollEnd = setTimeout(() => {
-			waitUntilScrollEnd = undefined
-		}, 32)
+		userIsScrolling = setTimeout(unsetIsScrolling, 32)
 	}
+
 	window.addEventListener('deviceorientation', rotationHandler, true)
 	window.addEventListener('scroll', scrollHandler, true)
 })()
