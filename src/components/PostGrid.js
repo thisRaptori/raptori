90be27942f4a1b4201d0fde58418c9cfcb7379ff
Joe McGrath <joe@raptori.dev>
2019-06-16 21:21:19 +0100

    add uncommitted component

---
+++ src/components/PostGrid.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 6rem;
+	margin-top: 3rem;
+
+	a {
+		margin-top: 0 !important;
+	}
+`
