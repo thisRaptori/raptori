8f26c8758b124c8e41e64d1a175be6cdb0fa06bb
Joe McGrath <joe@raptori.dev>
2019-06-22 21:27:23 +0100

    split image into two separate components

--- src/primitives/Image.js
+++ src/primitives/Image.js
@@ -4,47 +4,22 @@ import Img from 'gatsby-image'
 
 import { get } from 'src/utils'
 
-const Image = ({ height, name, width, style = {}, ...props }) => (
-	<StaticQuery
-		query={graphql`
-			query {
-				images: allFile {
-					edges {
-						node {
-							relativePath
-							name
-							childImageSharp {
-								fluid(maxWidth: 600) {
-									...GatsbyImageSharpFluid_noBase64
-								}
-							}
-						}
-					}
-				}
-			}
-		`}
-		render={data => {
-			const images = get(data, 'images', 'edges') || []
-			const image = images.find(({ node }) => {
-				return node.name === name || node.relativePath.includes(name)
-			})
+const Image = ({ height, image, style, width }) => {
+	const combinedStyles = { ...style }
+	if (width) {
+		combinedStyles.width = width
+	}
+	if (height) {
+		combinedStyles.height = height
+	}
 
-			const combinedStyles = { ...style }
-			if (width) {
-				combinedStyles.maxWidth = width
-			}
-			if (height) {
-				combinedStyles.maxHeight = height
-			}
+	return image ? (
+		<Img
+			fluid={get(image, 'node', 'childImageSharp', 'fluid')}
+			style={combinedStyle}
+			{...props}
+		/>
+	) : null
+}
 
-			return image ? (
-				<Img
-					fluid={get(image, 'node', 'childImageSharp', 'fluid')}
-					style={combinedStyles}
-					{...props}
-				/>
-			) : null
-		}}
-	/>
-)
 export default Image
---
+++ src/primitives/QueryImage.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import Image from './Image'
+import { get } from 'src/utils'
+
+const QueryImage = ({ name, ...props }) => (
+	<StaticQuery
+		query={graphql`
+			query {
+				images: allFile {
+					edges {
+						node {
+							relativePath
+							name
+							childImageSharp {
+								fluid(maxWidth: 600) {
+									...GatsbyImageSharpFluid_noBase64
+								}
+							}
+						}
+					}
+				}
+			}
+		`}
+		render={data => {
+			const images = get(data, 'images', 'edges') || []
+			const image = images.find(({ node }) => {
+				return node.name === name || node.relativePath.includes(name)
+			})
+
+			return <Image image={image} {...props} />
+		}}
+	/>
+)
+export default QueryImage
