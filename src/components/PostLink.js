a3b75ce3912f71d5139d3a24f132502fa85a5aee
Joe McGrath <joe@raptori.dev>
2019-06-23 22:08:41 +0100

    tweak PostLink layout

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -5,12 +5,19 @@ import { Image, Link, MetaText } from 'src/components'
 
 const StyledLink = styled(Link)`
 	color: var(${props => (props.inFooter ? '--dark' : '--text')}) !important;
-	display: block;
+	display: flex;
+	flex-direction: column;
 	margin: 1rem -1rem -1rem;
 	overflow: hidden;
 	padding: 1rem;
 	position: relative;
 	text-decoration: none;
+
+	> :last-child {
+		align-items: flex-end;
+		display: flex;
+		flex: 1 1 100%;
+	}
 
 	${props =>
 		!props.inFooter
@@ -39,6 +46,7 @@ const StyledLink = styled(Link)`
 		props.featured
 			? `
 		grid-column-end: span 2;
+		min-height: 230px;
 		padding: 2rem 1.5rem 2rem calc(50% + 5rem);
 
 		.gatsby-image-wrapper {
@@ -52,7 +60,7 @@ const StyledLink = styled(Link)`
 	`
 			: `
 		.gatsby-image-wrapper {
-			height: 160px;
+			flex: 0 0 160px;
 			margin: -1rem -1rem 1rem -1rem;
 		}
 		`}
@@ -68,13 +76,18 @@ const StyledLink = styled(Link)`
 	> ${MetaText} {
 		margin-top: 1rem;
 	}
+
+	hr {
+		border: none;
+		margin-top: 2rem;
+	}
 `
 
 const PostLink = ({
 	featured,
 	post: {
 		fields: { readingTime },
-		frontmatter: { date, featuredImage, path, subtitle, tags, title },
+		frontmatter: { featuredImage, path, subtitle, tags, title },
 	},
 	inFooter,
 }) => (
@@ -85,10 +98,9 @@ const PostLink = ({
 	>
 		<Image image={featuredImage} />
 		<h4>{title}</h4>
-		<MetaText italic>
-			{date} • {readingTime.text}
-		</MetaText>
 		{subtitle ? <MetaText>{subtitle}</MetaText> : null}
+		<hr />
+		<MetaText italic>{readingTime.text}</MetaText>
 	</StyledLink>
 )
 
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -34,7 +34,6 @@ export const pageQuery = graphql`
 					id
 					excerpt(pruneLength: 250)
 					frontmatter {
-						date(formatString: "MMMM DD, YYYY")
 						featuredImage {
 							childImageSharp {
 								fluid {
